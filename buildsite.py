b33831bd809f07ee213be26a52e48f3ba4718c18
andydevs <akanshul97@gmail.com>
2020-10-12 19:10:11 -0400

    Create site object which manages building

--- buildsite.py
+++ buildsite.py
@@ -11,35 +11,7 @@ import codecs
 import jinja2
 
 
-class Config:
-    """
-    Configuration of site builder
-    So I can keep it all in a namespace
-    """
-    templates_dir = 'templates'
-    notebook_dir = 'notebook'
-    output_dir = 'docs'
-
-    @property
-    def jinja_env(self):
-        """
-        Return jinja2 environment for this config
-        """
-        return jinja2.Environment(
-            loader=jinja2.FileSystemLoader(self.templates_dir),
-            autoescape=jinja2.select_autoescape(['html'])
-        )
-
-
-def make_directory(config):
-    """
-    Ensure that directory exists
-    """
-    if not os.path.exists(f'./{config.output_dir}'):
-        os.mkdir(f'{config.output_dir}')
-
-
-def build_notebooks(config):
+def build_notebooks(site):
     """
     Build jupyter notebooks
     """
@@ -47,11 +19,11 @@ def build_notebooks(config):
     html = nbconvert.HTMLExporter()
 
     # Loop through each file
-    for filename in glob.glob(f'{config.notebook_dir}/*.ipynb'):
+    for filename in glob.glob(f'{site.notebook_dir}/*.ipynb'):
         # Get output filename
         name = os.path.basename(filename)
         name = os.path.splitext(name)[0]
-        outn = f'{config.output_dir}/{name}.html'
+        outn = f'{site.output_dir}/{name}.html'
 
         # Read file and convert
         body, _ = html.from_filename(filename)
@@ -65,36 +37,86 @@ def build_notebooks(config):
             out.write(encoded)
 
 
-def build_index_page(config):
+def build_index_page(site):
     """
     Build index page
     """
     # Get template
-    template = config.jinja_env.get_template('index.html')
+    template = site.jinja_env.get_template('index.html')
 
     # Notebooks array
-    notebooks = glob.glob(f'{config.notebook_dir}/*.ipynb')
+    notebooks = glob.glob(f'{site.notebook_dir}/*.ipynb')
     notebooks = [ os.path.basename(file) for file in notebooks ]
     notebooks = [ os.path.splitext(name)[0] for name in notebooks ]
 
     # Render and write to file
     output = template.render(notebooks=notebooks)
-    with open(f'{config.output_dir}/index.html', 'w+') as file:
+    with open(f'{site.output_dir}/index.html', 'w+') as file:
         file.write(output)
 
 
-def build_utility(config):
+def build_utility(site):
     """
     Build utility files
     """
     # Write nojekyll file
-    with open(f'{config.output_dir}/.nojekyll', 'w+') as f:
+    with open(f'{site.output_dir}/.nojekyll', 'w+') as f:
         f.write('')
 
 
+class Site:
+    """
+    Site object, handles all of the building
+    """
+    # Default configuration for site
+    _defaults = {
+        'templates_dir': 'templates',
+        'notebook_dir': 'notebook',
+        'output_dir': 'docs',
+        'builders': [
+            build_notebooks,
+            build_index_page,
+            build_utility
+        ]
+    }
+
+    def __init__(self, **kwargs):
+        """
+        Initalize site object with given configuration,
+        passed through **kwargs
+
+        :see Site._defaults:
+        """
+        for key, value in self._defaults.items():
+            setattr(self, key, kwargs.get(key, value))
+
+    @property
+    def jinja_env(self):
+        """
+        Return jinja2 environment for this config
+        """
+        return jinja2.Environment(
+            loader=jinja2.FileSystemLoader(self.templates_dir),
+            autoescape=jinja2.select_autoescape(['html'])
+        )
+
+    def build(self):
+        """
+        Build site
+        """
+        self._make_directory()
+        for run_builder in self.builders:
+            run_builder(self)
+
+    def _make_directory(self):
+        """
+        Ensure that output directory exists
+        """
+        if not os.path.exists(f'./{self.output_dir}'):
+            os.mkdir(f'{self.output_dir}')
+
+
+
 if __name__ == "__main__":
-    config = Config()
-    make_directory(config)
-    build_notebooks(config)
-    build_index_page(config)
-    build_utility(config)
+    my_site = Site()
+    my_site.build()
