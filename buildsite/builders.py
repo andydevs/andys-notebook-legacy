b5f5c28740bb82f6640cb26dd684d9d0ec7032dc
Anshul Kharbanda <akanshul97@gmail.com>
2020-10-17 18:46:45 -0400

    Created builder superclass

--- buildsite/builders.py
+++ buildsite/builders.py
@@ -11,63 +11,79 @@ import markdown
 from . import notebook
 
 
-class NotebookBuilder:
-    def build(self, site):
-        log = logging.getLogger('build_notebooks')
+class Builder:
+    config_vars = []
 
+    def __repr__(self):
+        config = { k:getattr(self,k) for k in self.config_vars }
+        cfgstr = ', '.join(f'{k}={repr(v)}' for k,v in config.items())
+        return f'{type(self).__name__}({cfgstr})'
+
+    @property
+    def log(self):
+        return logging.getLogger(f'{self}.build')
+
+    def build(self, site):
+        raise NotImplementedError
+
+
+class NotebookBuilder(Builder):
+    def build(self, site):
         # Create exporter
         html = nbconvert.HTMLExporter(
             extra_loaders=[site.jinja_loader],
             template_file='notebook.html')
-        log.debug(f'HTMLExporter: {repr(html)}')
+        self.log.debug(f'HTMLExporter: {repr(html)}')
 
         # Export notebooks
         notebooks = notebook.load_all(site)
         for nb in notebooks:
-            log.info(f"Building '{nb.filename}'")
+            self.log.info(f"Building '{nb.filename}'")
             nb.export(html, site)
 
 
-class IndexBuilder:
+class IndexBuilder(Builder):
     output_name = 'index.html'
     index_tamplate_name = 'index.html'
 
     def build(self, site):
-        log = logging.getLogger('build_index_page')
-        log.info(f"Building '{self.output_name}'")
+        self.log.info(f"Building '{self.output_name}'")
 
         # Get template
         template = site.jinja_env.get_template(self.index_tamplate_name)
 
         # Get notebook data
         notebooks = notebook.load_all(site)
-        notebooks = [ notebook.get_notebook_data(site) for notebook in notebooks ]
-        log.debug(f'Notebooks data: {notebooks}')
+        notebooks = [ 
+            notebook.get_notebook_data(site) 
+            for notebook in notebooks ]
+        self.log.debug(f'Notebooks data: {notebooks}')
 
         # Read README markdown file
-        log.debug(f'Reading README.md')
+        self.log.debug(f'Reading README.md')
         with open('README.md', 'r') as f:
             text = f.read()
         readme = markdown.markdown(text)
-        log.debug(f'Readme: \n' + readme)
+        self.log.debug(f'Readme: \n' + readme)
 
         # Render and write to file
-        log.debug('Writing to output file')
+        self.log.debug('Writing to output file')
         output = template.render(readme=readme, notebooks=notebooks)
         with open(f'{site.output_dir}/{self.output_name}', 'w+') as file:
             file.write(output)
 
 
-class UtilityBuilder:
+class UtilityBuilder(Builder):
+    config_vars = ['filename']
+
     def __init__(self, filename):
         self.filename = filename
 
     def build(self, site):
-        log = logging.getLogger(f"build_utility('{self.filename}')")
-        log.info(f"Building '{self.filename}'")
+        self.log.info(f"Building '{self.filename}'")
 
         # Write nojekyll file
         outname = f'{site.output_dir}/{self.filename}'
-        log.debug(f'Writing to {outname}')
+        self.log.debug(f'Writing to {outname}')
         with open(outname, 'w+') as f:
             f.write('')
